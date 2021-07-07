676bda82f826bc42c3674f79f828166993f456da
SilentDubich <kirill.dubov.2012@mail.ru>
2021-07-07 08:35:54 +0300

    calendar init

--- src/components/content/training/training-item.tsx
+++ src/components/content/training/training-item.tsx
@@ -1,7 +1,8 @@
-import React, {FC} from "react";
+import React, {FC, useState} from 'react';
 import {addTrainingThunk, ApproachType, TrainingType} from '../../../data-base/reducers/training-reducer';
 import Styles from "./training-item.module.css"
 import {ApproachItem} from "./approach/approach-item";
+import {ReusableInputDateTime} from '../../reusable/reusable-input-date-time/reusable-input-date-time';
 
 
 type PropsType = {
@@ -22,12 +23,22 @@ export const TrainingItem:FC<PropsType> = ({ training, deleteTrainingThunk, addA
 		key={approach.order * Math.random()}
 		approach={approach}
 		trainingId={id}
-	/>)
+	/>);
+
+	const [ editTime, setEditTme ] = useState<boolean>(false);
+
 	return (
 		<div className={Styles.container}>
 			<div className={Styles.training}>
 				<div className={Styles.training_title}>{ title ? title : 'Без названия' }</div>
-				<div className={Styles.datetime}>{ normalizedDatetime }</div>
+				{ !editTime && <div className={Styles.datetime} onClick={() => setEditTme(true)}>{normalizedDatetime}</div> }
+				{
+					editTime &&
+					<div className={Styles.datetime}>
+						<ReusableInputDateTime></ReusableInputDateTime>
+						<div onClick={() => setEditTme(false)}>Сохранить</div>
+					</div>
+				}
 				<div className={Styles.delete} onClick={() => deleteTrainingThunk(id)}>Удалить</div>
 			</div>
 			<div className={Styles.description}>{ description ? `Описание тренировки: ${ description }` : 'Описание отсутствует' }</div>
